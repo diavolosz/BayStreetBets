a75f973f26c4c311a0b816a312b1c2542931cd17
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-07 03:22:27 -0400

    Replace passwords with bcrypt hashes

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- seeds/01_users.sql
 -- users seeds
-INSERT INTO users (email, username, password) VALUES ('ryan@gmail.com', 'ryan', '12345678');
-INSERT INTO users (email, username, password) VALUES ('michael@gmail.com', 'michael', '12345678');
-INSERT INTO users (email, username, password) VALUES ('sidney@gmail.com', 'sidney', '12345678');
+INSERT INTO users (email, username, password) VALUES ('ryan@gmail.com', 'ryan', '$2a$10$FhYvFknTdlrfOd1xlgPuL.oWbJp7XtWe8nIdKXpzykHUsaKEc9Mki');
+INSERT INTO users (email, username, password) VALUES ('michael@gmail.com', 'michael', '$2a$10$FhYvFknTdlrfOd1xlgPuL.oWbJp7XtWe8nIdKXpzykHUsaKEc9Mki');
+INSERT INTO users (email, username, password) VALUES ('sidney@gmail.com', 'sidney', '$2a$10$FhYvFknTdlrfOd1xlgPuL.oWbJp7XtWe8nIdKXpzykHUsaKEc9Mki');
