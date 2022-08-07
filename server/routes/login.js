17bcea06b4227aac2d2d6c7a9e5e586870d95977
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-07 14:39:52 -0400

    Remove console.log

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,21 +1,22 @@
-const router = require('express').Router();
-module.exports = (db) => {
-
-  router.post('/', (req, res) => {
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+module.exports = db => {
+  router.post("/", (req, res) => {
     const email = req.body["0"].value;
     const password = req.body["1"].value;
 
-    db.query(`SELECT * FROM users WHERE email = $1 AND password = $2;`, [email, password])
-    .then((result) => {
+    db.query(`SELECT * FROM users WHERE email = $1;`, [email]).then(result => {
       if (result.rows.length) {
-        // const accessToken = jwt.sign(
-        //   result.rows[0], process.env.ACCESS_TOKEN_SECRET
-        //   );
-        return res.json({ user: result.rows[0].id });
-      }  
+        if (bcrypt.compareSync(password, result.rows[0].password)) {
+          // const accessToken = jwt.sign(
+          //   result.rows[0], process.env.ACCESS_TOKEN_SECRET
+          //);
+          return res.json({ user: result.rows[0].id });
+        }
+      }
       return res.json({ user: null });
-    })
+    });
   });
 
   return router;
-}
+};
