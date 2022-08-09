d8b801bcc479b3da502efea051868e3839c5e3bf
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-09 13:36:22 -0400

    Implement basic POST endpoint for handling creation of competitions

--- server/routes/competitions.js
+++ server/routes/competitions.js
@@ -9,5 +9,31 @@ module.exports = db => {
 
   }); 
 
+  router.post("/", (req, res) => {
+    const name = req.body["0"].value;
+    const description = req.body["1"].value;
+    const starting_amount = req.body["2"].value;
+    const start_date = req.body["4"][0].slice(0, 10);
+    const end_date = req.body["4"][1].slice(0, 10);
+    const user_id = req.body["user"];
+
+    // Check if user is logged in
+    if (!user_id) {
+      return res.sendStatus(401);
+    }
+
+    return db.query(`INSERT INTO competitions(name, description, starting_amount, start_date, end_date, created_date, user_id) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *;`, [name, description, starting_amount, start_date, end_date, new Date(), user_id])
+    .then(result => {
+      db.query(`INSERT INTO user_competitions(user_balance, user_id, competition_id) VALUES($1, $2, $3) RETURNING *;`, [starting_amount, user_id, result.rows[0]["id"]])
+    .then(() => {
+        return res.sendStatus(201);
+    })
+    .catch((err) => {
+      return res.send(err);
+    });
+    });
+
+  }); 
+
   return router;
 };
