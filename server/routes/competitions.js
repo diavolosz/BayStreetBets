75e9ba365a0a5ea81daf2e99c50868ed23992b9e
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-10 03:58:18 -0400

    Add jwt verification to POST route for creating competitions; implement DELETE route for deleting competitions

--- server/routes/competitions.js
+++ server/routes/competitions.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 
 module.exports = db => {
   router.get("/", (req, res) => {
@@ -15,23 +16,30 @@ module.exports = db => {
     const starting_amount = req.body["2"].value;
     const start_date = req.body["4"][0].slice(0, 10);
     const end_date = req.body["4"][1].slice(0, 10);
-    const user_id = req.body["user"];
+    const userJwt = req.body["user"];
 
     // Check if user is logged in
-    if (!user_id) {
-      return res.sendStatus(401);
-    }
+    jwt.verify(userJwt, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      // Return 401 if JWT verification fails
+      if (err) {
+        return res.sendStatus(401);
+      }
 
-    return db.query(`INSERT INTO competitions(name, description, starting_amount, start_date, end_date, created_date, user_id) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *;`, [name, description, starting_amount, start_date, end_date, new Date(), user_id])
+      // Perform database operations
+      return db.query(`INSERT INTO competitions(name, description, starting_amount, start_date, end_date, created_date, user_id) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *;`, [name, description, starting_amount, start_date, end_date, new Date(), user.id])
       .then(result => {
-        db.query(`INSERT INTO user_competitions(user_balance, user_id, competition_id) VALUES($1, $2, $3) RETURNING *;`, [starting_amount, user_id, result.rows[0]["id"]])
+        db.query(`INSERT INTO user_competitions(user_balance, user_id, competition_id) VALUES($1, $2, $3) RETURNING *;`, [starting_amount, user.id, result.rows[0]["id"]])
           .then(() => {
-            return res.sendStatus(201);
+            return res.json(result);
           })
           .catch((err) => {
             return res.send(err);
           });
       });
+
+    });
+
+    
   });
 
   router.post("/user_competitions", (req, res) => {
@@ -44,18 +52,33 @@ module.exports = db => {
   });
 
   router.delete("/", (req, res) => {
-    // const {user_id, competition_id } = req.options.data;
-    // // Check if user is logged in
+    const userJwt = req.headers.user;
+    const { competitionId, creatorId } = req.body;
 
-    // // Delete competition from competitions and user_competitions
-    // return db.query(`DELETE FROM competitions WHERE id = $1 AND user_id = $2;`, [competition_id, user_id])
-    // .then(result => {
-    //   db.query(`DELETE FROM user_competitions WHERE competition_id = $1;`, [competition_id])
-    // .then(() => {
-    //   return res.sendStatus(200);
-    // })
-    // .catch(() => res.sendStatus(403));
-    // });
+    // Check if user is logged in
+    jwt.verify(userJwt, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(401);
+      }
+
+      // Check if user making the delete request is the creator of the competition
+      if (user.id !== creatorId) {
+        return res.sendStatus(401);
+      }
+
+      // Delete competition from competitions and user_competitions
+      return db.query(`DELETE FROM competitions WHERE id = $1;`, [competitionId])
+      .then(result => {
+        db.query(`DELETE FROM user_competitions WHERE competition_id = $1;`, [competitionId])
+      .then(() => {
+        return res.sendStatus(200);
+      })
+      .catch(() => res.sendStatus(403));
+      });
+
+    });
+
+    
   });
 
   return router;
