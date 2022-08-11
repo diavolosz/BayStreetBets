6a912cf399152b7a9723aa7030b4279dbc7029fc
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-10 20:02:22 -0400

    Change query for fetching competitions enrolled

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,15 +1,12 @@
 const router = require("express").Router();
 
 module.exports = db => {
-  router.get("/", (req, res) => {
-    
-
-  }); 
+  router.get("/", (req, res) => {});
 
   router.get("/:id", (req, res) => {
     const { id } = req.params;
     db.query(`SELECT * FROM users WHERE id = $1;`, [id]).then(result => {
-      const user = {...result.rows[0]}
+      const user = { ...result.rows[0] };
       delete user.password;
       return res.json(user);
     });
@@ -19,14 +16,17 @@ module.exports = db => {
     const { id } = req.params;
     let competitions = {};
     db.query(`SELECT * FROM competitions WHERE user_id = $1;`, [id])
-    .then(result => {
-      competitions["competitionsCreated"] = result.rows;
-      return db.query(`SELECT * FROM competitions JOIN user_competitions ON competitions.id = user_competitions.competition_id WHERE user_competitions.user_id = $1;`, [id]);
-    })
-    .then((result) => {
-      competitions["competitionsEnrolled"] = result.rows;
-      res.json(competitions);
-    });
+      .then(result => {
+        competitions["competitionsCreated"] = result.rows;
+        return db.query(
+          `SELECT * FROM competitions JOIN user_competitions ON competitions.id = user_competitions.competition_id WHERE user_competitions.user_id = $1 AND competitions.user_id != $2;`,
+          [id, id]
+        );
+      })
+      .then(result => {
+        competitions["competitionsEnrolled"] = result.rows;
+        res.json(competitions);
+      });
   });
 
   return router;
