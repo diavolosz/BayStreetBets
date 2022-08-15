d44888851ac07b2fc7c4719e5d1260f8496193f2
le duong hien thong <michaeldiavolos@gmail.com>
2022-08-15 17:46:29 -0400

    fixed css styling and added browse event seeds

--- server/db/seeds/03_transactions.sql
+++ server/db/seeds/03_transactions.sql
@@ -11,3 +11,16 @@ INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction
 INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Buy', 'AMZN', 200.00, 20, '2022-08-12 00:00:00-05', 2, 1);
 INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Buy', 'EW', 300.00, 5, '2022-08-12 00:00:00-05', 2, 1);
 
+
+
+-- INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Buy', 'BNGO', 100.00, 8, '2022-08-02 00:00:00-05', 2, 3);
+-- INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Buy', 'TSLA', 200.00, 8, '2022-08-03 00:00:00-05', 2, 3);
+-- INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Buy', 'HOOD', 150.00, 18, '2022-08-04 00:00:00-05', 2, 3);
+-- INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Buy', 'NFLX', 125.00, 18, '2022-08-07 00:00:00-05', 2, 3);
+-- INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Sell', 'HOOD', 140.00, -18, '2022-08-09 00:00:00-05', 2, 3);
+-- INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Sell', 'BNGO', 110.00, -8, '2022-08-10 00:00:00-05', 2, 3);
+-- INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Buy', 'AMD', 100.00, 18, '2022-08-10 00:00:00-05', 2, 3);
+-- INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Sell', 'NFLX', 130.00, -8, '2022-08-12 00:00:00-05', 2, 3);
+-- INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Buy', 'AMZN', 200.00, 28, '2022-08-12 00:00:00-05', 2, 3);
+-- INSERT INTO transactions (buy_sell, symbol, price, number_of_shares, transaction_date, user_id, competition_id) VALUES ('Buy', 'EW', 300.00, 8, '2022-08-12 00:00:00-05', 2, 3);
+
