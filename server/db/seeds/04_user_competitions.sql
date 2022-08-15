464e11b1256a867dbf15c1b07704848d4b7d5e8f
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-14 22:39:16 -0400

    Revise seeds

--- server/db/seeds/04_user_competitions.sql
+++ server/db/seeds/04_user_competitions.sql
@@ -1,9 +1,14 @@
 -- seeds/user_competitions.sql
 -- user_competitions seeds
-INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (10000.00, 2, 1);
-INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (20000.00, 2, 2);
+INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (10000.00, 1, 1);
+INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (10000.00, 2, 2);
+INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (50000.00, 3, 3);
+INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (20000.00, 1, 4);
 
+INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (10000.00, 2, 1);
 INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (50000.00, 2, 3);
+
+
 
 UPDATE user_competitions SET user_balance = 9500.00 WHERE user_id = 2 AND competition_id = 1;
 UPDATE user_competitions SET user_balance = 8500.00 WHERE user_id = 2 AND competition_id = 1;
