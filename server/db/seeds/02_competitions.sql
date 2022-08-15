ce4bc7d6ac6f32de2ba52163ad9dd1c6818c9783
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-15 14:01:30 -0400

    Revise query

--- server/db/seeds/02_competitions.sql
+++ server/db/seeds/02_competitions.sql
@@ -1,6 +1,6 @@
 -- seeds/02_competitions.sql
 -- competitions seeds
-INSERT INTO competitions (name, description, starting_amount, start_date, end_date, created_date, user_id) VALUES ('Tech stocks only', 'Three-week competition for trading tech stocks!', 10000.00, '2022-08-01 00:00:00-05', '2022-08-25 00:00:00-05', '2022-07-30 00:00:00-05', 1);
+INSERT INTO competitions (name, description, starting_amount, start_date, end_date, created_date, user_id) VALUES ('Tech stocks only', 'Three-week competition for trading tech stocks!', 10000.00, '2022-08-01 00:00:00-05', '2022-08-25 00:00:00-05', '2022-07-30 00:00:00-05', 2);
 
 INSERT INTO competitions (name, description, starting_amount, start_date, end_date, created_date, user_id) VALUES ('ETFs!', 'Test your trading skills', 20000.00, '2022-11-11 00:00:00-05', '2022-11-18 00:00:00-05', '2022-08-02 00:00:00-05', 2);
 
--- server/db/seeds/04_user_competitions.sql
+++ server/db/seeds/04_user_competitions.sql
@@ -1,6 +1,6 @@
 -- seeds/user_competitions.sql
 -- user_competitions seeds
-INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (10000.00, 1, 1);
+INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (10000.00, 1, 2);
 INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (20000.00, 2, 2);
 INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (50000.00, 3, 3);
 INSERT INTO user_competitions (user_balance, user_id, competition_id) VALUES (50000.00, 1, 4);
